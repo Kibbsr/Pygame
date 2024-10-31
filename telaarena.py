cfb89bd90b94f43d9b8a3371254c98581902eaa6
erick <erickmm@al.insper.edu.br>
2024-10-31 10:59:56 -0300

    arena

--- telaarena.py
+++ telaarena.py
@@ -5,10 +5,15 @@ largura_tela = 1550
 altura_tela = 835
 janela = pygame.display.set_mode((largura_tela, altura_tela))
 pygame.display.set_caption("")
-fundo = pygame.image.load("arenaluta.png")
+fundo = pygame.image.load("c:/Users/erick/Downloads/arenaluta.png").convert_alpha()
 fundo = pygame.transform.scale(fundo, (largura_tela, altura_tela))
+def plano():
+    janela.blit(fundo,(0,0))
 ini=True
 while ini:
+    plano()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            ini==False
+            ini=False
+    pygame.display.update()
+pygame.quit()
