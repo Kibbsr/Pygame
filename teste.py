ed35734f6755f09eb343a9486396b7a9adbd13c9
erick <erickmm@al.insper.edu.br>
2024-10-31 16:00:12 -0300

    hit box

--- teste.py
+++ teste.py
@@ -2,17 +2,16 @@ import pygame
 
 pygame.init()
 
-lutador_img = pygame.image.load("").convert_alpha()
-lutador_img = pygame.transform.scale(lutador_img, (#dimensões,))
+#lutador_img = pygame.image.load("").convert_alpha()
+#lutador_img = pygame.transform.scale(lutador_img,)#dimensões,))
 
-class Lutador(pygame.sprite.Sprite):
-    def __init__(self, img):
-        pygame.sprite.Sprite.__init__(self)
-
-        self.image = img  
-        self.rect = self.image.get_rect()
-        self.rect.x  = ##
-        self.rect.bottom = ##
-        self.speedx = 0
-
-    def update(self):
+class Lutador():
+    def __init__(self,x,y):
+       
+       # pygame.sprite.Sprite.__init__(self)
+        self.rect = pygame.Rect((x,y,50,150))
+        #self.rect.x  = 
+        #self.rect.bottom = 
+        #self.speedx = 0
+    def box(self,surface):
+        pygame.draw.rect (surface, (0,0,255), self.rect)
