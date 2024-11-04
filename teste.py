7c079daeb395eb583b2e42e39b5c960dc68e1348
mateusas3 <mateusas3@al.insper.edu.br>
2024-11-04 16:36:05 -0300

    nao passar da tela

--- teste.py
+++ teste.py
@@ -1,6 +1,6 @@
 import pygame
 
-
+largura_tela = 1550
 class Lutador():
     def __init__(self,x,y):
 
@@ -16,6 +16,12 @@ class Lutador():
             dimensao_x=mov_velocidade
         self.rect.x +=dimensao_x
         self.rect.y +=dimensao_y
+        if self.rect.right > largura_tela:
+            self.rect.right = largura_tela
+        if self.rect.left < 0:
+            self.rect.left = 0
+
+        
     def movimentação2(self):
         mov_velocidade=3
         dimensao_x=0
@@ -27,5 +33,10 @@ class Lutador():
             dimensao_x=-mov_velocidade
         self.rect.x +=dimensao_x
         self.rect.y +=dimensao_y
+        if self.rect.right > largura_tela:
+            self.rect.right = largura_tela
+        if self.rect.left < 0:
+            self.rect.left = 0
+
     def box(self,surface):
         pygame.draw.rect (surface, (0,0,255),self.rect)
