109a136dc83fdd6a2a196838515374b0fc7e1b6c
Kibssr <kikes4606@gmail.com>
2024-11-06 15:47:22 -0300

    AAAA

--- selecaotela.py
+++ selecaotela.py
@@ -8,18 +8,18 @@ janela = pygame.display.set_mode((largura_tela, altura_tela))
 pygame.display.set_caption("")
 fundo = pygame.image.load("ARENA.png").convert()
 fundo = pygame.transform.scale(fundo, (largura_tela, altura_tela))
+tela = pygame.image.load("arenaluta.png").convert()
 def plano():
     janela.blit(fundo,(0,0))
 
-lutador1=Lutador(1000, 600)
-lutador2=Lutador(100, 600)
+lutador1=Lutador(100, 600)
+lutador2=Lutador(1000, 600)
 ini=True
 while ini:
     for event in pygame.event.get():
         plano()
-        if event.type == pygame.KEYDOWN:
-            fundo = pygame.image.load("arenaluta2.png").convert()
-            janela.blit(fundo, (500,200))
+        if event.type == pygame.KEYUP:
+            tela.blit(fundo,(500,200))
         if event.type == pygame.QUIT:
             ini=False
     pygame.display.update()
