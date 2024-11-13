d274bccdd9145588f643f99d594be3e3148cb162
mateusas3 <mateusas3@al.insper.edu.br>
2024-11-13 16:17:05 -0300

    começando tela de seleção

---
+++ tela_selecao_personagens.py
@@ -0,0 +1,35 @@
+import pygame
+from teste import Lutador
+import subprocess
+import sys
+
+
+pygame.init()
+largura_tela = 1550
+altura_tela = 835
+janela = pygame.display.set_mode((largura_tela, altura_tela))
+pygame.display.set_caption("Seleção Personagens")
+
+pos1 = ()
+
+#Importar lutadores
+
+personagem1 = 
+personagem2 = 
+personagem3 =
+personagem4 =
+
+personagens = [personagem1, personagem2, personagem3, personagem4]
+
+
+ini=True
+while ini:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            ini = False
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+
+
+
+        
+    
